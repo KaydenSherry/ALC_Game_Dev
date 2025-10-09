ddf46208613195b79fc8ad95f24ff807f731ddac
KaydenSherry <kayden.s720@stu.nebo.edu>
2025-10-09 09:22:08 -0600

    hfgj

---
+++ 2D Platformer2/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerController2D : MonoBehaviour
+{
+    //Value Types
+    [Header("Player Settings")]
+    public float moveSpeed;
+    public float jumpForce;
+    public bool isGrounded;
+    public int bottomBound;
+    [Header("Score")]
+    public int score;
+
+
+    //Reference Types
+    public Rigidbody2D rig;
+    public TextMeshProUGUI scoreText;
+
+    //Increase the score and Update the score text UI
+    public void AddScore(int amount)
+    {
+        score += amount;
+        scoreText.text = "Score: " + score;
+    }
+
+    void FixedUpdate()
+    {
+        float moveInput = moveInput.GetAxisRaw("Horizontal");
+        rig.linearVelocity = new Vector2(moveInput * moveSpeed, rig.linearVelocity.y);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            isGrounded = false;
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (transform.position.y < bottomBound)
+        {
+            GameOver();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.GetContact(0).normal == Vector2.up)
+        {
+            isGrounded = true;
+        }
+    }
+    
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
